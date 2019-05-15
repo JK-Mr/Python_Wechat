dc3174b03f1d9576bf294765a26b91fd0c6289b5
jiang <13681020078@163.com>
2019-05-15 15:46:36 +0800

    python定时发送

---
+++ run.py
@@ -0,0 +1,19 @@
+"""
+@author: Jiang Ke
+@license: Apache Licence 
+@contact: jiangke9413@qq.cpm
+@site: 
+@software: PyCharm
+@file: run.py
+@time: 2019/4/8 13:24
+"""
+
+from Wechat.GFWeather import GFWeather
+
+
+def run():
+    GFWeather().run()
+
+if __name__ == '__main__':
+    # test_run()
+    run()
